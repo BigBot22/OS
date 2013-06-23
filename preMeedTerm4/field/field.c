bc82c28ad972f9fb39b93442b6c7d5ad588f8af9
Bogdan <kail-stail@mail.ru>
2013-06-23 19:18:20 +0400

    Working version of field.c

--- preMeedTerm4/field/field.c
+++ preMeedTerm4/field/field.c
@@ -16,190 +16,169 @@ int sepCount;
 
 void showRow();
 
-int Reading(){
+int Reading()
+{
+    _Bool colonNotEnded = false;
+    _Bool lastIt = false;
+    int curSep = 0;
 
-	_Bool colonNotEnded = false;
-	_Bool lastIt = false;
-	int curSep = 0;
+    filled = 0;
+    while(1)
+    { 
+        //printf("while\n");
+        int readed = read(0, inBuf + filled, max - filled) - 1; // minus '\n' from the end
+        if (readed == 0 || readed > max) {
+            //printf("readed 0");
+            if(filled == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                lastIt = true;
+            }
+        }
 
-	filled = 0;
-	while(1)
-	{ 
-		//printf("while\n");
-		int readed = read(0, inBuf + filled, max - filled);
-		if (readed == 0 || readed > max) {
-			//printf("readed 0");
-			if(filled == 0)
-			{
-				return 0;
-			}
-			else
-			{
-				lastIt = true;
-			}
-		}
-		
-		filled += readed;
-		
-		char * s = strstr(inBuf, sep[curSep]);
-		while(s != NULL)
-		{
+        filled += readed;
+        char * s = strstr(inBuf, sep[curSep]);
+        while(s != NULL)
+        {
 
-			int i = 0;
-			int sPos = s - inBuf;
-			printf("sepPos:%d\n", sPos);
-			for(i = 0; i < sPos; ++i)
-			{
-				out[curSep][i] = inBuf[i];
-			}
-			out[curSep][i] = '\0';
-			printf("out[%d]:%s\n",curSep, out[curSep]);
-			
-			filled = filled - (sPos + sepLen[curSep]);
-			memmove(inBuf, s + sepLen[curSep], filled);
-			
-			printf("inBuf:%s\n", inBuf);
-			
-			if(curSep == sepCount - 1)
-			{
-				showRow();
-				printf("ok6");
-				curSep = 0;
-			}
-			else
-			{
-				curSep++;
-			}
+            int i = 0;
+            int sPos = s - inBuf;
+            //printf("sepPos:%d\n", sPos);
+            for(i = 0; i < sPos; ++i)
+            {
+                out[curSep][i] = inBuf[i];
+            }
+            out[curSep][i] = '\0';
+            //printf("out[%d]:%s\n",curSep, out[curSep]);
+            filled = filled - (sPos + sepLen[curSep]);
+            memmove(inBuf, s + sepLen[curSep], filled);
 
-			s = strstr(inBuf, sep[curSep]);	
-		}
-		
-		if(!lastIt && filled < max)
-		{
-			//continue;
-		}
-		else
-		{
-			return -1;
-		}
-		
-		
-	}
-		
-	return 1;
+            //printf("inBuf:%s\n", inBuf);
+            if(curSep == sepCount - 1)
+            {
+                showRow();
+                curSep = 0;
+            }
+            else
+            {
+                curSep++;
+            }
+
+            s = strstr(inBuf, sep[curSep]);	
+        }
+
+        if(!lastIt && filled < max)
+        {
+            //continue;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 1;
 }
 
 void showRow()
 {
-	printf("ok7");
-	printf("ok7.3");
-	
-	int i;
-	for(i = 0; i < sepCount; ++i)
-	{
-		printf("ok8");
-		printf("%s ", out[i]);
-		printf("ok9");
-	}
+    printf(">> ");
 
-	printf("ok10");
-	for(i = 0; i < sepCount; ++i)
-	{
-		printf("%s ", out[order[i]]);
-	}
-	
+    int i;
+
+    for(i = 0; i < sepCount; ++i)
+    {
+        printf("%s ", out[order[i] - 1]);
+    }
+    printf("\n");
+
 }
 
 
 
 int main(int argc, char * argv[])
 {
-	
-	printf("begin\n");
+    if(argc > 1){
 
-	if(argc > 1){
-		
-		//printf("ok1\n");
-		sepCount = 0;
-		int i;
-		for(i = 1 ; argv[i][0] != ';'; ++i)
-		{
-			//printf("ok2 %d\n", i);
-			sepCount++;
-		}
+        //printf("ok1\n");
+        sepCount = 0;
+        int i;
+        for(i = 1 ; argv[i][0] != ';'; ++i)
+        {
+            //printf("ok2 %d\n", i);
+            sepCount++;
+        }
 
-		//printf("ok3\n");
-		//printf("sepCount:%d\n", sepCount);
+        //printf("ok3\n");
+        //printf("sepCount:%d\n", sepCount);
 
-		sep = malloc(sepCount);
-		out = malloc(sepCount);
-		sepLen = malloc(sepCount);
-		order = malloc(sepCount);
-		max = 128;
-		
-		for(i = 0; i < sepCount; ++i)
-		{
-			sep[i] = malloc(max);
-			out[i] = malloc(max);
-			sepLen[i] = 0;
-			int j;
-			for(j = 0; argv[i + 1][j] != '\0'; ++j)
-			{
-				sep[i][j] = argv[i + 1][j];	
-				sepLen[i]++;
-			}
-			sep[i][j] = '\0';
-		}
-		
-		//printf("ok4\n");
-		++i;
-		++i;
-		int j = 0;
-		order[j] = 0;
-		for(i = i; i < argc; ++i)
-		{
-			int l;
-			for(l = 0; argv[i][l] != '\0'; ++l)
-			{
-				//printf("a:%d\n",argv[i][l] );
-				order[j] = order[j] * 10 + argv[i][l] - '0';
-			}
-			j++;
-			//printf("\n\n\n");
-		}
-		//printf("ok5\n");
-	}
-	else
-	{
-		printf("wrong Args!\n");
-		return 0;
-	}
-	
-	
-	
-	printf("begin\n");
-	int i;
-	for(i = 0; i < sepCount; ++i)
-	{
-		printf("sep:%s sepLen:%d ord:%d\n", sep[i], sepLen[i], order[i]);
-	}
+        sep = malloc(sepCount);
+        out = malloc(sepCount);
+        sepLen = malloc(sepCount);
+        order = malloc(sepCount);
+        max = 128;
+
+        for(i = 0; i < sepCount; ++i)
+        {
+            sep[i] = malloc(max);
+            out[i] = malloc(max);
+            sepLen[i] = 0;
+            int j;
+            for(j = 0; argv[i + 1][j] != '\0'; ++j)
+            {
+                sep[i][j] = argv[i + 1][j];	
+                sepLen[i]++;
+            }
+            sep[i][j] = '\0';
+        }
+        
+        //printf("ok4\n");
+        ++i;
+        ++i;
+        int j = 0;
+        order[j] = 0;
+        for(i = i; i < argc; ++i)
+        {
+            int l;
+            for(l = 0; argv[i][l] != '\0'; ++l)
+            {
+                //printf("a:%d\n",argv[i][l] );
+                order[j] = order[j] * 10 + argv[i][l] - '0';
+            }
+            j++;
+            //printf("\n\n\n");
+        }
+        //printf("ok5\n");
+    }
+    else
+    {
+        printf("wrong Args!\n");
+        return 0;
+    }
+
+    int i;
+    for(i = 0; i < sepCount; ++i)
+    {
+        //printf("sep:%s sepLen:%d ord:%d\n", sep[i], sepLen[i], order[i]);
+    }
 
 
-	inBuf = malloc(max);
-	//outBuf = malloc(size);
+    inBuf = malloc(max);
+    //outBuf = malloc(size);
 
-	Reading(); 
+    Reading(); 
 
-	free(inBuf);
-	//free(outBuf);
+    free(inBuf);
+    //free(outBuf);
 
-	for(i = 0; i < sepCount; ++i)
-	{
-		free(sep[i]);
-		free(out[i]);
-	}
-	free(sep);
-	free(out);
-	return 0; 
+    for(i = 0; i < sepCount; ++i)
+    {
+        free(sep[i]);
+        free(out[i]);
+    }
+    free(sep);
+    free(out);
+    return 0; 
 }
- 
